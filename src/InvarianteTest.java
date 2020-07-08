79f1dfc8bb5fea48245825ed1a395309ba29b9e8
tomsar <tsarquis88@gmail.com>
2020-07-08 14:59:16 -0300

    avance en regex

--- src/InvarianteTest.java
+++ src/InvarianteTest.java
@@ -8,50 +8,18 @@ public class InvarianteTest {
     static void checkInvariantes(String invariantesFile, int[][] invariantes)
     {
         String transString = getTransicionesString(invariantesFile);
-        String aux = "";
-        String regex = "";
-        char parOpen = '(';
-        char parClose = ')';
+
         int i;
-        int digit;
-        boolean end = false;
-
-        for(i = 0; i < invariantes[0].length; i++)
+        for( i = 0; i < invariantes.length; i++ )
         {
-            if( invariantes[0][i] == -1 )
-                break;
-            else
-            {
-                if( invariantes[0][i] > 9 )
-                    digit = invariantes[0][i] + 55;
-                else
-                    digit = invariantes[0][i] + 48;
-                regex = regex.concat( String.valueOf( (char) digit ) );
-
-                if( i < invariantes[0].length - 1)
-                {
-                    regex = regex.concat( String.valueOf( parOpen ) );
-                    regex = regex.concat( ".*" );
-                    regex = regex.concat( String.valueOf( parClose ) );
-                }
-            }
+            String regex = getRegex(invariantes[i]);
+            String replacement = getReplacement( regex );
+            transString = replace(regex, replacement, transString);
+            transString = cleanString(transString, "#");
         }
 
-        System.out.println( regex );
+        transString = cleanString(transString, " ");
         System.out.println( transString );
-        do
-        {
-            aux = transString.replaceAll( regex, "#$1#$2#$3#$4#$5#$6#");
-
-            if( aux.equalsIgnoreCase( transString ) )
-                end = true;
-            else
-            {
-                transString = aux;
-                System.out.println( transString );
-            }
-        } while( !end );
-
 
     }
 
@@ -74,4 +42,81 @@ public class InvarianteTest {
         return transiciones;
     }
 
+    static String getRegex( int[] invariante ) {
+        int i, len, digit;
+        String regex = "";
+        char parOpen = '(';
+        char parClose = ')';
+
+        for (len = 0; len < invariante.length; len++) {
+            if (invariante[len] == -1)
+                break;
+        }
+
+        for (i = 0; i < len; i++)
+        {
+            if (invariante[i] > 9)
+                digit = invariante[i] + 55;
+            else
+                digit = invariante[i] + 48;
+            regex = regex.concat(String.valueOf((char) digit));
+
+            if (i < len - 1) {
+                regex = regex.concat(String.valueOf(parOpen));
+                regex = regex.concat(".*");
+                regex = regex.concat(String.valueOf(parClose));
+            }
+        }
+        return regex;
+    }
+
+    static String getReplacement( String regex )
+    {
+        int i, c = 0;
+        String replacement = "";
+
+        for(i = 0; i < regex.length(); i++)
+        {
+            if( regex.charAt( i ) == '*' )
+                c++;
+        }
+        for(i = 0; i < c; i++)
+        {
+            replacement = replacement.concat( "#$" + (i + 1) );
+        }
+        replacement = replacement.concat( "#" );
+
+        return replacement;
+    }
+
+    static String replace(String regex, String replacement, String text)
+    {
+        String aux;
+        boolean end = false;
+
+        System.out.println( "\n" + regex );
+        System.out.println( replacement + "\n" );
+        System.out.println( text );
+
+        do
+        {
+            aux = text.replaceAll( regex, replacement);
+
+            if( aux.equalsIgnoreCase( text ) )
+                end = true;
+            else
+            {
+                text = aux;
+                System.out.println( text );
+            }
+        } while( !end );
+
+        return text;
+    }
+
+    static String cleanString(String text, String durt)
+    {
+        text = text.replaceAll(durt, "");
+        return text;
+    }
 }
